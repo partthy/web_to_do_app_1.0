52a9bcbc6e3a306ee9edd1a5eccccca56283b458
partthy <partthy@gmail.com>
2023-09-28 23:50:33 +0530

    Implemented Complete item function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
+
 import functions
 
 todos = functions.get_todo_list()
+
 
 def add_todo():
     todo_local = st.session_state["new_todo"] + "\n"
@@ -14,8 +16,13 @@ st.title('_My Todo App_ is :blue[cool] :sunglasses:')
 st.subheader("This is my todo app", divider="rainbow")
 st.write("This app is to help people remember things to do")
 
-for todo in todos:
-    st.checkbox(todo)
-
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todo_list(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Add a Todo", placeholder="Add a new todo item...", on_change=add_todo, key="new_todo")
+
